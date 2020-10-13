561b71a6d15aee75a6f57a69c2aa4806382cb32a
miguelangel.navarro <miguelangel.navarro@bi-geek.com>
2020-10-13 18:09:05 +0200

    upload console app project.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var line = Console.ReadLine();
+            Console.WriteLine("Hello " + line);
+        }
+    }
+}
